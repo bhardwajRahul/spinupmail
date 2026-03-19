5141a8924247ed6c6b64676dde438e107659363e
Rıdvan Aktogan <63660173+ridvan@users.noreply.github.com>
2026-03-19 22:34:05 +0300

    feat: add dashboard command menu (#78)
    * feat: add dashboard command menu
    
    * fix: unit tests
    
    * fixes for reviews

--- packages/frontend/src/test/setup.ts
+++ packages/frontend/src/test/setup.ts
@@ -1,7 +1,34 @@
 import { afterAll, afterEach, beforeAll, vi } from "vitest";
 import { server } from "@/test/msw/server";
 
+class ResizeObserverMock {
+  observe() {}
+
+  unobserve() {}
+
+  disconnect() {}
+}
+
 beforeAll(() => {
+  if (typeof window !== "undefined" && !("ResizeObserver" in window)) {
+    Object.defineProperty(window, "ResizeObserver", {
+      configurable: true,
+      writable: true,
+      value: ResizeObserverMock,
+    });
+  }
+
+  if (
+    typeof Element !== "undefined" &&
+    !("scrollIntoView" in Element.prototype)
+  ) {
+    Object.defineProperty(Element.prototype, "scrollIntoView", {
+      configurable: true,
+      writable: true,
+      value: () => {},
+    });
+  }
+
   server.listen({ onUnhandledRequest: "error" });
 });
 
